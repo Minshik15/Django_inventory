ad76d1d57da9f85b347f2abd50a272cd196c60e7
Adidas <geegii.huslen@gmail.com>
2025-05-15 19:57:13 +0800

    Added Render domain to ALLOWED_HOSTS

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-2f*zznxxdc22&5zngq0edusr=7ayj3k=2_xtxbz^)@ff6e@zl(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',
+    '127.0.0.1',
+    'django-inventory-y033.onrender.com',]
 
 
 # Application definition
